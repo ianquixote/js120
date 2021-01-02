8cf9d470a9b68bebe7e9e5157636ea8f28b631e9
Ian Austin <ianaustin2560@gmail.com>
2021-01-03 00:37:26 +0700

    Updated OORPS.js

--- lesson1/OORPS.js
+++ lesson1/OORPS.js
@@ -1,40 +1,40 @@
 const readline = require('readline-sync');
-const MAX_PTS = 5;
-const VALID_INPUT = {
-  rock: ['ro', 'roc', 'rock'],
-  paper: ['pa', 'pap', 'paper'],
-  scissors: ['sc', 'sci', 'scissors'],
-  lizard: ['li', 'liz', 'lizard'],
-  spock: ['sp', 'spo', 'spock'],
-};
-
-const LOSERS = {
-  rock:     ['scissors', 'lizard'],
-  paper:    ['rock',     'spock'],
-  scissors: ['paper',    'lizard'],
-  lizard:   ['paper',    'spock'],
-  spock:    ['rock',     'scissors'],
-};
-
-const WINNERS = {
-  rock:     ['paper',    'spock'],
-  paper:    ['scissors', 'lizard'],
-  scissors: ['rock',     'spock'],
-  lizard:   ['scissors', 'rock'],
-  spock:    ['lizard',   'paper'],
-};
 
 const RPSGame = {
   human: createHuman(),
   computer: createComputer(),
   score: {human: 0, computer: 0},
+  rounds: 5,
+  winners: {
+    rock:     ['paper',    'spock'],
+    paper:    ['scissors', 'lizard'],
+    scissors: ['rock',     'spock'],
+    lizard:   ['scissors', 'rock'],
+    spock:    ['lizard',   'paper'],
+  },
+  losers: {
+    rock:     ['scissors', 'lizard'],
+    paper:    ['rock',     'spock'],
+    scissors: ['paper',    'lizard'],
+    lizard:   ['paper',    'spock'],
+    spock:    ['rock',     'scissors'],
+  },
+  validInput: {
+    rock: ['ro', 'roc', 'rock'],
+    paper: ['pa', 'pap', 'paper'],
+    scissors: ['sc', 'sci', 'scissors'],
+    lizard: ['li', 'liz', 'lizard'],
+    spock: ['sp', 'spo', 'spock'],
+  },
 
   displayWelcomeMessage() {
+    console.clear();
     console.log('Welome to Rock, Paper, Scissors, Lizard, Spock!');
-    console.log(`Win ${MAX_PTS} rounds to win the game.`);
+    console.log(`Win ${this.rounds} rounds to win the game.`);
   },
 
   displayGoodbyeMessage() {
+    console.clear();
     console.log('Thank you for playing Rock, Paper, Scissors!');
   },
 
@@ -42,7 +42,7 @@ const RPSGame = {
     let humanMove = this.human.move;
     let computerMove = this.computer.move;
     console.log(`You chose ${humanMove} and the computer chose ${computerMove}.`);
-    if (playerWins(humanMove, computerMove)) {
+    if (this.playerWins(humanMove, computerMove)) {
       this.score.human += 1;
       console.log(`You won the round.\nSCORE - Human: ${this.score.human}, Computer: ${this.score.computer}.`);
     } else if (humanMove === computerMove) {
@@ -75,25 +75,28 @@ const RPSGame = {
     console.log(moveString);
   },
 
-  playAgain() {
-    if (this.score.human === MAX_PTS) {
+  displayMatchWinner() {
+    if (this.score.human === this.rounds) {
       console.log('\nYou win the match!!!');
     } else {
       console.log('\nThe computer wins the Match!!!');
     }
-    console.log('\nWould you like to play again? (yes/no)');
-    let answer = readline.question();
+  },
 
-    let isValid = ['y', 'yes', 'yep', 'yeah', 'yay',
-      'n', 'no', 'nope', 'nah', 'nay'].includes(answer.toLowerCase());
+  playAgain() {
+    console.log('\nWould you like to play again? (yes/no)');
+    let answer = readline.question().toLowerCase();
+
+    let validAnswers = ['y', 'yes', 'yep', 'yeah', 'yay',
+      'n', 'no', 'nope', 'nah', 'nay'];
+    let isValid = validAnswers.includes(answer);
     while (!isValid) {
       console.log('Sorry, that is invalid. Please enter yes or no.');
       answer = readline.question();
-      isValid = ['y', 'yes', 'yep', 'yeah', 'yay',
-        'n', 'no', 'nope', 'nah', 'nay'].includes(answer.toLowerCase());
+      isValid = validAnswers.includes(answer);
     }
 
-    return ['y', 'yes', 'yep', 'yeah', 'yay'].includes(answer.toLowerCase());
+    return ['y', 'yes', 'yep', 'yeah', 'yay'].includes(answer);
   },
 
   updateHumanMovePercentage() {
@@ -110,22 +113,32 @@ const RPSGame = {
     this.human.movePercentage.spock = (spMoves / totalMoves) * 100;
   },
 
-  play() {
+  playerWins(choice, computerChoice) {
+    return this.losers[choice].includes(computerChoice);
+  },
+
+  newMatch() {
     console.clear();
+    console.log('New Match!');
+  },
+
+  play() {
     this.displayWelcomeMessage();
     while (true) {
       this.score = {human: 0, computer: 0};
-      while (this.score.human < MAX_PTS && this.score.computer < MAX_PTS) {
-        this.human.choose();
-        this.computer.choose(this.human.movePercentage);
+      while (this.score.human < this.rounds &&
+             this.score.computer < this.rounds) {
+        this.human.choose(this.validInput);
+        this.computer.choose(this.human.movePercentage, this.winners);
         console.clear();
         this.updateAndDisplayScore();
         this.displayMoves('human');
         this.displayMoves('computer');
         this.updateHumanMovePercentage();
-        // console.log(this.human.movePercentage);
       }
+      this.displayMatchWinner();
       if (!this.playAgain()) break;
+      this.newMatch();
     }
     this.displayGoodbyeMessage();
   }
@@ -134,7 +147,7 @@ const RPSGame = {
 function createComputer() {
   let playerObject = createPlayer();
   let computerObject = {
-    choose(humanMovePercentages) {
+    choose(humanMovePercentages, winnerObj) {
       //Rule: Use the percentages of the human player's moves to represent
       //the likelihood that the computer will choose the move which beats
       //that move. For example, if the human chooses rock 80% of the time,
@@ -146,19 +159,19 @@ function createComputer() {
       let random2 = Math.floor(Math.random() * 2);
       if (random1 <= weights.rock) {
         //this.move beats rock
-        this.move = WINNERS.rock[random2];
+        this.move = winnerObj.rock[random2];
       } else if (random1 > weights.rock && random1 <= weights.paper) {
         //this.move beats paper
-        this.move = WINNERS.paper[random2];
+        this.move = winnerObj.paper[random2];
       } else if (random1 > weights.paper && random1 <= weights.scissors) {
         //this.move beats scissors
-        this.move = WINNERS.scissors[random2];
+        this.move = winnerObj.scissors[random2];
       } else if (random1 > weights.scissors && random1 <= weights.lizard) {
         //this.move beats lizard
-        this.move = WINNERS.lizard[random2];
+        this.move = winnerObj.lizard[random2];
       } else {
         //this.move beats spock
-        this.move = WINNERS.spock[random2];
+        this.move = winnerObj.spock[random2];
       }
       this.moves[this.move] += 1;
     }
@@ -172,17 +185,19 @@ function createHuman() {
   let humanObject = {
     movePercentage: {rock: 20, paper: 20, scissors: 20, lizard: 20, spock: 20},
 
-    choose() {
+    choose(validInput) {
       let input;
       let choice = null;
       while (!choice) {
         input = readline.question('\nPlease choose rock, paper, scissors, ' +
-        'lizard, or spock (ro/pa/sc/li/sp).\n');
-        Object.keys(VALID_INPUT).forEach((key) => {
-          if (VALID_INPUT[key].includes(input.toLowerCase())) {
-            choice = key;
+        'lizard, or spock (ro/pa/sc/li/sp).\n').toLowerCase();
+        let validInputKeys = Object.keys(validInput);
+        for (let key = 0; key < validInputKeys.length; key++) {
+          let current = validInputKeys[key];
+          if (validInput[current].includes(input)) {
+            choice = current;
           }
-        });
+        }
         console.log('Sorry, invalid choice. Try again.');
       }
       this.move = choice;
@@ -211,10 +226,6 @@ function createWeights(humanMovePercentages) {
   obj.lizard = 100 - spPercent;
   obj.spock = 100;
   return obj;
-}
-
-function playerWins(choice, computerChoice) {
-  return LOSERS[choice].includes(computerChoice);
 }
 
 RPSGame.play();
